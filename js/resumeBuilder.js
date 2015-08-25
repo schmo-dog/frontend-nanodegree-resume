009f167b4f0c06e6d083516ab8d2d1bac1b6b276
schmo-dog <aaron.schmohe@gmail.com>
2015-08-25 10:12:05 -0600

    Updated the index file and resumebuilder.js file with the latest excercises.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,33 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+	var name = "Aaron Schmohe";
+	var role = "Web Developer";
+
+	var formattedName = HTMLheaderName.replace("%data%", name);
+
+
+	var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+	$("#header").prepend(formattedRole);
+	$("#header").prepend(formattedName);
+  
+ 
+ var bio = {
+ 	"name" : "Aaron Schmohe",
+ 	"role" : "Web Developer",
+ 	"contacts" : {
+ 		"phone" : "720.934.8439",
+ 		"eamil" : "aaron.schmohe@gmail.com"
+ 	},
+	"WelcomeMessage" : "lorem ipsum emet etc etc.",
+ 	skills : ["CSS", "HTML5", "Adobe CC", "JS", "Jquery"],
+ 	"bioPic" : "images/aaron.jpg"
+ };
+
+ 	var formattedMobile = HTMLmobile .replace("%data%", bio.role);
+
+//$("#main").append(bio.name);
+
+$("#header").prepend(formattedMobile);
